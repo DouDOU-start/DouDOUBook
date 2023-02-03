2308178bd70601484a3a99c74cfc2046eb745733
huangenjun <huangenjun@genew.com>
2023-02-03 17:25:59 +0800

    微信双开脚本

---
+++ tools/windows 微信双开.bat
@@ -0,0 +1,9 @@
+@REM 通过start多次exe文件实现微信双开，start "" "此处替换为微信可执行文件地址"
+
+@echo off
+
+start "" "D:\Program Files\Tencent\WeChat\WeChat.exe"
+
+start "" "D:\Program Files\Tencent\WeChat\WeChat.exe"
+
+exit
